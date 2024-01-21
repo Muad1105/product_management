33723e5562e63312d0e4e0b9f584e385cad0a5b6
muad1105 <muadnoushad@gmail.com>
2024-01-22 00:56:06 +0530

    add wishlist controller created, usermodel where wishlist and otp data added

---
+++ backend/controller/user/wishlist/setWishlistController.js
@@ -0,0 +1,33 @@
+import { User } from "../../../model/user/userModel.js";
+
+export const addWishlistItem = async (req, res, next) => {
+  const { productId, userId } = req.body;
+  console.log("req.body", req.body);
+
+  let user;
+  try {
+    user = await User.findById(userId);
+    console.log("user");
+    if (!user) {
+      return res.status(404).json({ message: "User Not Found." });
+    }
+
+    //check if the item already preesent
+    const itemPresent = user.wishlist.includes(productId);
+    if (itemPresent) {
+      return res.status(409).json({ message: "Conflict, item present." });
+    }
+    //Add the item to the user wishlist array
+    user.wishlist.push(productId);
+
+    console.log("wishlist added");
+
+    await user.save();
+    console.log("user saved");
+    return res
+      .status(201)
+      .json({ message: "Wishlist Item Added Successfully" });
+  } catch (err) {
+    return res.status(500).json({ message: "Internal server error." });
+  }
+};
--- backend/model/user/userModel.js
+++ backend/model/user/userModel.js
@@ -4,6 +4,8 @@ const userSchema = mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true, minLength: 4 },
   email: { type: String, required: true, unique: true },
+  otp: { type: Number, required: false },
+  wishlist: [{ type: Number, required: false }],
 });
 
 export const User = mongoose.model("User", userSchema);
