9b63b94b5ac3815ee5b247e3d0e740f8ec32130c
muad1105 <muadnoushad@gmail.com>
2024-01-22 09:02:52 +0530

    backend, wishlist model removed

--- backend/model/user/wishlistModel.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const wishlistSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-});
-
-export const Wishlist = mongoose.model("Wishlist", wishlistSchema);
